a7ea772c1431a6aa50966b5c948f21351d30ae84
jopak19 <jpaulogomessiqueira@gmail.com>
2023-05-13 20:33:17 -0300

    arquitetura com serviços

--- non-reactive/game-service/src/main/java/br/ufrn/dimap/collaborativecanvas/gameservice/controlers/GameControler.java
+++ non-reactive/game-service/src/main/java/br/ufrn/dimap/collaborativecanvas/gameservice/controlers/GameControler.java
@@ -1,18 +1,12 @@
 package br.ufrn.dimap.collaborativecanvas.gameservice.controlers;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import br.ufrn.dimap.collaborativecanvas.gameservice.models.JogadaCanvaDTO;
-import br.ufrn.dimap.collaborativecanvas.gameservice.models.JogadaPlayerDTO;
+import br.ufrn.dimap.collaborativecanvas.gameservice.gameservice.GameService;
 import br.ufrn.dimap.collaborativecanvas.gameservice.models.PaintingDTO;
 
 
@@ -20,17 +14,18 @@ import br.ufrn.dimap.collaborativecanvas.gameservice.models.PaintingDTO;
 @RequestMapping("/play")
 public class GameControler {
 
+    @Autowired
+    public GameService gameService;
+
     @PostMapping
-    public ResponseEntity<PaintingDTO> getPlayerById(@RequestBody PaintingDTO paint) {
+    public ResponseEntity<PaintingDTO> play(@RequestBody PaintingDTO paint) {
+        PaintingDTO jogada = gameService.play(paint);
+        if (jogada != null) {
+            return ResponseEntity.ok(paint);
+        } else {
+            return ResponseEntity.internalServerError().build();
+        }
         
-        RestTemplate restTemplate = new RestTemplate();
-        String jogadaURL = "http://localhost:8085/player/play";
-        String joagadaCanvaURL = "http://localhost:8093/painting";
-        HttpEntity<JogadaPlayerDTO> request = new HttpEntity<>(new JogadaPlayerDTO(paint.getPlayerId()));
-        HttpEntity<JogadaCanvaDTO> request2 = new HttpEntity<>(new JogadaCanvaDTO(paint.getPixelId(), paint.getPlayerId(), paint.getCanvaId(), paint.getcolor()));
-        ResponseEntity<Void> response = restTemplate.exchange(jogadaURL, HttpMethod.PUT, request, Void.class);
-        ResponseEntity<Void> response2 = restTemplate.exchange(joagadaCanvaURL, HttpMethod.POST, request2, Void.class);
-        return ResponseEntity.ok(paint);
     }
 
     
