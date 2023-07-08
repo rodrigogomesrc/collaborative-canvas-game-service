e7adab68244dc06cf82fbe27a94fbd91e17012be
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-07-08 17:46:37 -0300

    teste de adição de mensageria

--- reactive/reactive-game-service/src/main/java/br/ufrn/dimap/collaborativecanvas/reactivegameservice/ReactiveGameServiceApplication.java
+++ reactive/reactive-game-service/src/main/java/br/ufrn/dimap/collaborativecanvas/reactivegameservice/ReactiveGameServiceApplication.java
@@ -3,11 +3,20 @@ package br.ufrn.dimap.collaborativecanvas.reactivegameservice;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableWebFlux
 public class ReactiveGameServiceApplication {
+
+	/*
+	@Bean
+	public Supplier<String> helloworld() {
+		return () -> "Hello World";
+	}
+
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveGameServiceApplication.class, args);
